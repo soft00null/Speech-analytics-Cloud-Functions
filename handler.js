bde397371b692b2cc1abf7c5fee227ae15667fa8
Emiliano Pelliccioni <emilianop11@gmail.com>
2017-04-12 12:56:24 -0300

    Added google speech support

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hook = (event, context, callback) => {
+module.exports.voicebasehook = (event, context, callback) => {
   var AWS = require('aws-sdk');
   var request = require('request');
   AWS.config.update({ accessKeyId: process.env.SERVERLESS_AWS_ACCESS_KEY_ID, secretAccessKey: process.env.SERVERLESS_AWS_SECRET_ACCESS_KEY });
@@ -16,7 +16,7 @@ module.exports.hook = (event, context, callback) => {
 
   request(sendTranscriptOptions, function(error, response, body){
     if (!error)
-      console.log("Successfully sent transcript to callback url");
+      console.log("Successfully sent voicebase transcript to callback url");
   });
 
   var getFileOptions = {
@@ -57,6 +57,8 @@ module.exports.hook = (event, context, callback) => {
 };
 
 module.exports.voicebase = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({ accessKeyId: process.env.SERVERLESS_AWS_ACCESS_KEY_ID, secretAccessKey: process.env.SERVERLESS_AWS_SECRET_ACCESS_KEY });
   var request = require('request');
   console.log("New file in bucket voicebase-voxrec-poc");
   const RESOURCE_URL = 'https://s3.amazonaws.com/' + event.Records[0].s3.bucket.name + '/' + event.Records[0].s3.object.key;
@@ -126,4 +128,69 @@ module.exports.voicebase = (event, context, callback) => {
   form.append('media', RESOURCE_URL);
   form.append('configuration', JSON.stringify(configuration));
 
+  const FILE_NAME = event.Records[0].s3.object.key;
+
+  //trigger transcoding to flac
+  const elastictranscoder = new AWS.ElasticTranscoder();
+  const params = {
+    //audiotranscoder pipeline
+    PipelineId: '1491924039069-q5xvuc',
+    Input: {
+      AspectRatio: 'auto',
+      Container: 'auto',
+      FrameRate: 'auto',
+      Interlaced: 'auto',
+      Key: FILE_NAME,
+      Resolution: 'auto'
+    },
+    Outputs: [
+      {
+        Key: FILE_NAME + '.flac',
+        PresetId: '1491945029791-ltldjb'
+      }
+    ]
+  };
+
+  elastictranscoder.createJob(params, function(err, data) {
+    console.log("TRANSCODER RESPONSE");
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+};
+
+
+module.exports.googlespeech = (event, context, callback) => {
+  var speech = require('@google-cloud/speech');
+  var request = require('request');
+  const RESOURCE_URL = 'https://s3.amazonaws.com/' + event.Records[0].s3.bucket.name + '/' + event.Records[0].s3.object.key
+  console.log(RESOURCE_URL);
+
+  var speechClient = speech({
+    projectId: 'voxbone-workshop',
+    keyFilename: 'Workshop-c5795abec2f2.json'
+  });
+
+  const options = {
+    encoding: 'FLAC',
+    languageCode: 'en-US'
+  };
+
+  speechClient.recognize(RESOURCE_URL, options)
+  .then((results) => {
+    const transcription = results[0];
+    var sendTranscriptOptions = {
+      url: process.env.TRANSCRIPT_CALLBACK_URL,
+      method: 'POST',
+      json: transcription
+    };
+
+    console.log(sendTranscriptOptions);
+
+    request(sendTranscriptOptions, function(error, response, body){
+      if (!error)
+        console.log("Successfully sent googlespeech transcript to callback url");
+      else
+        console.log(error);
+    });
+  });
 };
